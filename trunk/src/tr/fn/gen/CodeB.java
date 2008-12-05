97957cef07517bba892071e5bd2c77213979a561
eliksiir22@hotmail.com <eliksiir22@hotmail.com@5fa317ae-bce1-11dd-8164-2ff6f70dce53>
2008-12-05 15:50:46 +0000

    Implemented IfThenElse in CodeB.java

--- trunk/src/tr/fn/gen/CodeB.java
+++ trunk/src/tr/fn/gen/CodeB.java
@@ -8,6 +8,9 @@ import tr.fn.ast.op.BinaryOperator;
 import tr.fn.gen.instr.BinaryOpInstruction;
 import tr.fn.gen.instr.GetBasic;
 import tr.fn.gen.instr.LoadC;
+import tr.fn.gen.instr.Jumpz;
+import tr.fn.gen.instr.Jump;
+import tr.fn.gen.instr.Label;
 
 public class CodeB {
 	
@@ -17,7 +20,6 @@ public class CodeB {
 	 * @see MaMa slides page 9.
 	 */
 	public static final void codeB(Environment environment, GenerationContext context, Expression e, int sd) throws GenerateException {
-		// FIXME implement if then else!!!
 		if (e instanceof Integer) {
 			codeBInteger(environment, context, (Integer) e, sd);
 		} else if (e instanceof BinaryOperator) {
@@ -25,7 +27,7 @@ public class CodeB {
 		} else if (e instanceof Identifier) {
 			codeBIdentifier(environment, context, (Identifier) e, sd);
 		} else if (e instanceof IfThenElse) {
-			throw new RuntimeException("unimplemented if-then");
+			codeBIfThenElse(environment, context, (IfThenElse) e, sd);
 		} else {
 			CodeV.codeV(environment, context, e, sd);
 			context.addInstruction(new GetBasic());
@@ -45,5 +47,18 @@ public class CodeB {
 
 	private static void codeBInteger(Environment environment, GenerationContext context, Integer e, int sd) {
 		context.addInstruction(new LoadC(e.getValue()));
+	}
+	
+	private static void codeBIfThenElse (Environment environment, GenerationContext context, IfThenElse e, int sd) throws GenerateException {
+		Label A = context.makeLabel();
+		Label B = context.makeLabel();
+		
+		codeB(environment, context, e.getCondition(), sd);
+		context.addInstruction(new Jumpz(A));
+		codeB(environment, context, e.getThenExpression(), sd);
+		context.addInstruction(new Jump(B));
+		context.addInstruction(A);
+		codeB(environment, context, e.getElseExpression(), sd);
+		context.addInstruction(B);
 	}
 }
