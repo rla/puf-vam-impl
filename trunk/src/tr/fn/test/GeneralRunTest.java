99d1020d976a79419c32491ef8e347119c89df74
eliksiir22@hotmail.com <eliksiir22@hotmail.com@5fa317ae-bce1-11dd-8164-2ff6f70dce53>
2008-12-30 12:44:03 +0000

    List creation with case statement.

--- trunk/src/tr/fn/test/GeneralRunTest.java
+++ trunk/src/tr/fn/test/GeneralRunTest.java
@@ -6,7 +6,23 @@ import junit.framework.TestCase;
 
 public class GeneralRunTest extends TestCase {
 	
+	public void testSingletonList() throws Exception {
+		assertEquals(3, RunUtil.compileAndRun(new File("test/singeltonlist.fn")));
+	}
+	
 	public void testTupleLet() throws Exception {
 		assertEquals(10, RunUtil.compileAndRun(new File("test/tuple3.fn")));
+	}
+	
+	public void testSelect1() throws Exception {
+		assertEquals(2, RunUtil.compileAndRun(new File("test/select/select1.fn")));
+	}
+	
+	public void testSelect2() throws Exception {
+		assertEquals(2, RunUtil.compileAndRun(new File("test/select/select2.fn")));
+	}
+	
+	public void testSelect3() throws Exception {
+		assertEquals(2, RunUtil.compileAndRun(new File("test/select/select3.fn")));
 	}
 }
